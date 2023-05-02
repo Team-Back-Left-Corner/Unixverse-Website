9a6b32802798a2f42182071ab890f993f7a03f2d
Cade Gilbert <98070090+WWG7211@users.noreply.github.com>
2023-05-01 23:56:26 -0400

    added about us

---
+++ about.php
@@ -0,0 +1,33 @@
+<?php
+	$path = "./";
+	include($path . "assets/inc/header.php");
+?>
+
+<div class="dashboard">
+        <div class="aboutus">
+            <h1>About the team</h1>
+            <hr>
+            <div class="people">
+                <div class="person">
+                    <h2>Jack Lindsey-noble</h2>
+                    <p>Our main dev and php officianto</p>
+                </div>
+                <div class="person">
+                    <h2>Cade Gilbert</h2>
+                    <p>Our content strategist</p>
+                </div>
+                <div class="person">
+                    <h2>Minh Nguyen</h2>
+                    <p>Our main designer</p>
+                </div>
+                <div class="person">
+                    <h2>Veronica Petti</h2>
+                    <p>Our accesability professional</p>
+                </div>
+            </div>
+        </div>
+    </div>
+	
+</body>
+<?php include($path . "assets/inc/footer.php");?>
+</html>
